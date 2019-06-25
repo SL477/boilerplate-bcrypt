95478f446b3adf37f4941132d7f7e164bcfc7b65
SL477 <fishwickt@gmail.com>
2019-06-25 20:41:23 +0100

    final bcrypt

--- server.js
+++ server.js
@@ -12,13 +12,38 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-var bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
+app.get("/", function (req, res) {
+  bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res);
+    });
+    
+    res.send(hash);
+  });
+});
 
+app.get("/password/:test", function (req, res) {
+  bcrypt.hash(req.params.test, saltRounds, (err, hash) => {
+    console.log(hash);
+    res.send(hash);
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
-
+app.get("/sync", function (req, res) {
+  var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+  console.log(hash);
+  
+  var result = bcrypt.compareSync(myPlaintextPassword, hash);
+  console.log(result);
+  
+  res.send(hash + " " + result);
+});
 
 
 //END_SYNC
